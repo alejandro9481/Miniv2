489e67184f65ff7f4ae23156a2bff3ce0d24b5da
Javier-C0L <javier.castrillon@correounivalle.edu.co>
2022-02-11 09:11:21 -0500

    prueba branch c

--- src/myProject/IKnowThatWord.java
+++ src/myProject/IKnowThatWord.java
@@ -1,5 +1,8 @@
 package myProject;
 
+/**
+ * Clase IKnowThatWord is the controller
+ */
 public class IKnowThatWord {
     private int tiempo, nivel, porcentaje, aciertos, score;
     //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

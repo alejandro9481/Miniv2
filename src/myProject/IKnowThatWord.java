42b3d91ca3d676bd4c226def3c999757be2f1e51
alejandro9481 <alejandovillamilcarmona@gmail.com>
2022-02-11 09:30:05 -0500

    creacion de rama alejandro

---
+++ src/myProject/IKnowThatWord.java
@@ -0,0 +1,9 @@
+package myProject;
+
+/**
+ * Clase IKnowThatWord is the controller
+ */
+public class IKnowThatWord {
+    private int tiempo, nivel, porcentaje, aciertos, score;
+    //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+}
